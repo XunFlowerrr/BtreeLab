5f6022b1c5c821f58b1eafec6ba6f87caca27e9d
XunFlowerrr <105523593+XunFlowerrr@users.noreply.github.com>
2024-08-28 02:59:32 +0700

    chore: Refactor precedence method in Utility class to use switch expression and remove unused method

--- src/Utility.java
+++ src/Utility.java
@@ -1,40 +1,15 @@
-import java.util.ArrayList;
-
 public class Utility {
     public static boolean isOperator(String token) {
         return token.equals("+") || token.equals("-") || token.equals("*") || token.equals("/") || token.equals("^");
     }
 
     public static int precedence(String token) {
-        if (token.equals("+") || token.equals("-")) {
-            return 1;
-        } else if (token.equals("*") || token.equals("/")) {
-            return 2;
-        } else if (token.equals("^")) {
-            return 3;
-        } else {
-            return 0;
-        }
+        return switch (token) {
+            case "+", "-" -> 1;
+            case "*", "/" -> 2;
+            case "^" -> 3;
+            default -> 0;
+        };
     }
-
-    public static int getClosureIndex(ArrayList<String> expression, int start) {
-        int end = 0;
-        int openParenthesis = 1;
-        int closeParenthesis = 0;
-
-        for (int j = start + 1; j < expression.size(); j++) {
-            if (expression.get(j).equals("(")) {
-                openParenthesis++;
-            } else if (expression.get(j).equals(")")) {
-                closeParenthesis++;
-            }
-            if (openParenthesis == closeParenthesis) {
-                end = j;
-                break;
-            }
-        }
-        return end;
-    }
-
 
 }
